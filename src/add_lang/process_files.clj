6d2580766307fa59de3d539535f0445196a0f13c
Arkadii Drovosekov <arkdro@protonmail.com>
2022-06-19 12:18:09 +0200

    only create a new file when it's needed

--- src/add_lang/process_files.clj
+++ src/add_lang/process_files.clj
@@ -37,17 +37,25 @@
   [text]
   (re-find abbrev_regex text))
 
-(defn get_new_content
+(defn get_content_and_change_flag
   [file]
-  (let [content (slurp file)]
+    (let [content (slurp file)]
     (if (need_change? content)
-      (change_content file content)
-      content)))
+      {:need_change true, :content content}
+      {:need_change false, :content nil})))
+
+(defn change_one_file
+  [file content]
+  (let [new_content (change_content file content)]
+    (write_output_file new_content file)))
 
 (defn process_one_file
   [file]
-  (let [new_content (get_new_content file)]
-    (write_output_file new_content file)))
+  (let [{need_change :need_change
+         content :content} (get_content_and_change_flag file)]
+    (if need_change
+      (change_one_file file content)
+      nil)))
 
 (defn process_files
   [files]
